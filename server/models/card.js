baaaa14b88bab1e0eb0a33925382c350605ef6c6
Peter Kim <peter-minho-kim@gmail.com>
2019-01-21 17:01:32 -0500

    Adds POST /cards route and refactors mongoose

---
+++ server/models/card.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+
+const Card = mongoose.model('Card', {
+  text: {
+    type: String,
+    required: true,
+    minlength: 1,
+    trim: true
+  },
+  completed: {
+    type: Boolean,
+    default: false
+  },
+  completedAt: {
+    type: Number,
+    default: null
+  }
+})
+
+module.exports = { Card }
---
+++ server/server.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const { mongoose } = require('./db/mongoose')
+const { Card } = require('./models/card')
+const { User } = require('./models/user')
+
+const app = express()
+
+app.use(bodyParser.json())
+
+app.post('/cards', (req, res) => {
+  const card = new Card({
+    text: req.body.text
+  })
+
+  card.save().then((doc) => {
+    res.send(doc)
+  }, (err) => {
+    res.status(400).send(err)
+  })
+})
+
+app.listen(8080, () => {
+  console.log('The magic happens on port 8080')
+})

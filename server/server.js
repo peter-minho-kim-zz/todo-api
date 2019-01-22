81c09caffdb2a40c06857bc92cf9e728b19cfc5a
Peter Kim <peter-minho-kim@gmail.com>
2019-01-22 18:38:07 -0500

    Adds POST /users route and token method

--- server/server.js
+++ server/server.js
@@ -96,6 +96,19 @@ app.patch('/cards/:id', (req, res) => {
   })
 })
 
+app.post('/users', (req, res) => {
+  const body = _.pick(req.body, ['email', 'password'])
+  const user = new User(body)
+
+  user.save().then(() => {
+    return user.generateAuthToken()
+  }).then((token) => {
+    res.header('x-auth', token).send(user)
+  }).catch((err) => {
+    res.status(400).send(err)
+  })
+})
+
 app.listen(port, () => {
   console.log(`The magic happens on port ${port}`)
 })
